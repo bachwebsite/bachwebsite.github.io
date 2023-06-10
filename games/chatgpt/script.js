b5b07edc5842055303a97eda48594637c10cb5da
bachwebsite <126933766+bachwebsite@users.noreply.github.com>
2023-06-09 23:14:01 -0500

    Update script.js

--- games/chatgpt/script.js
+++ games/chatgpt/script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "PASTE-YOUR-API-KEY-HERE"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
