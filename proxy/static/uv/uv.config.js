8a7a8ea80645d7da0f1a416fc598e8aa309abe7c
latvia <126933766+bachwebsite@users.noreply.github.com>
2023-08-26 00:17:44 -0500

    Update uv.config.js

--- proxy/static/uv/uv.config.js
+++ proxy/static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
+    prefix: '/proxy/static/tiw/',
     bare: 'https://bare.starttiw.org',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: '/proxy/static/uv/uv.handler.js',
+    bundle: '/proxy/static/uv/uv.bundle.js',
+    config: '/proxy/static/uv/uv.config.js',
+    sw: '/proxy/static/uv/uv.sw.js',
 };
