5a28cafd08862c99fee9c74d9f0e20452872f57d
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-05 12:51:03 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/astro/',
-    bare:'https://math-english-sci.vercel.app/',
+    bare:'/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
