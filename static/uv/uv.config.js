95f7afd1bea31977f43aeae761d93691ee49864e
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-04 22:12:09 -0500

    fsd

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/static/assignments/',
+    prefix: '/=/assignments/',
     bare:'/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: '/=/uv/uv.handler.js',
+    bundle: '/=/uv/uv.bundle.js',
+    config: '/=/uv/uv.config.js',
+    sw: '/=/uv/uv.sw.js',
 };
