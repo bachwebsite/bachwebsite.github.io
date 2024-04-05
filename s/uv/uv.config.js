2a4cd0a05edff03148aa7b17fd4ab8c0fd6b95ac
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-04 22:21:57 -0500

    eq to s

--- s/uv/uv.config.js
+++ s/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/=/assignments/',
+    prefix: '/s/assignments/',
     bare:'/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/=/uv/uv.handler.js',
-    bundle: '/=/uv/uv.bundle.js',
-    config: '/=/uv/uv.config.js',
-    sw: '/=/uv/uv.sw.js',
+    handler: '/s/uv/uv.handler.js',
+    bundle: '/s/uv/uv.bundle.js',
+    config: '/s/uv/uv.config.js',
+    sw: '/s/uv/uv.sw.js',
 };
--- s/uv/uv.sw.js
+++ s/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
+importScripts('/s/uv/uv.bundle.js');
+importScripts('/s/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
