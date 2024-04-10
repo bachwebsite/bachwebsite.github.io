d9c04be732f77c6e037a88ddd87641b46e0e8479
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-09 21:52:31 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/static/b/',
+    prefix: '/static/tutors/',
     bare:'https://dm-unbl0cker-bare-server.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
