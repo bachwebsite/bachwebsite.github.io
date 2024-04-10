dff1b553a53b34b23003b41c1daa0fd625440376
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-04-09 19:00:33 -0700

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/static/tutors/',
+    prefix: '/static/b/',
     bare:'https://dm-unbl0cker-bare-server.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
