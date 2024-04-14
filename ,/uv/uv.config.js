aa3a43aa92e52effef9541f8a0579b91143bafae
latvia <126933766+bachwebsite@users.noreply.github.com>
2024-04-13 19:53:57 -0500

    Update uv.config.js

--- ,/uv/uv.config.js
+++ ,/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/,/~/',
-    bare:'https://breakium.pw/',
+    bare:'http://bare.mza.com.ar',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/,/uv/uv.handler.js',
